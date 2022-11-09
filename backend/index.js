26d75093ba58a93be01171ff9169bd3a7b38184b
taojouet <taojouet@gmail.com>
2022-11-09 20:06:05 +0100

    Add endpoint teams & players

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,16 @@ app.get('/', (req, res) => {
   res.send('Hello World, welcome to sport stats!');
 });
 
+app.get('/api/teams', (req, res) => {
+  // TODO: Get teams from database
+  res.send('Teams');
+});
+
+app.get('/api/players', (req, res) => {
+  // TODO: Get players from database
+  res.send('Players');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
